20b60e6601ea422bb7eb6ce2254e5f1600e23c69
Jeremy D. Jones <j.jones1232@gmail.com>
2023-02-14 20:47:27 -0700

    Fix tabular formating of plain text data

--- src/app/loot-generator/models/loot.model.ts
+++ src/app/loot-generator/models/loot.model.ts
@@ -22,6 +22,8 @@ export class Loot {
 
   public toString(): string {
     let data: string = '';
+    const col1Width: number = 16;
+    const col2Width: number = 90;
 
     if (this.coins.length > 0) {
       data += 'Coins\nCoin\tAmount\n'
@@ -31,10 +33,10 @@ export class Loot {
     })
 
     if (this.treasures.length > 0) {
-      data += "Treasure\nType\tName\tValue\n";
+      data += `Treasure\n${'Type'.padEnd(col1Width)}${'Name'.padEnd(col2Width)}Value\n`;
     }
     this.treasures.forEach( treasure => {
-      data += `${TreasureType[treasure.type]}\t${treasure.name}\t${treasure.value === 0 ? '-' : treasure.value}\n`
+      data += `${TreasureType[treasure.type].padEnd(col1Width)}${treasure.name.padEnd(col2Width)}${treasure.value === 0 ? '-' : treasure.value}\n`
     })
 
     return data;
